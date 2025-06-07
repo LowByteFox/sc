0747f7997933e2306df1a7c858680e6a9854b1a8
LowByteFox <firethefox77@gmail.com>
2025-06-07 09:30:50 +0200

    c3 project init

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-CC="${CC:-cc}"
-OPT="${OPT:--O0}"
-
-NOWARNS="-Wno-dangling-pointer -Wno-unused-parameter"
-
-"$CC" -std=c11 -static -Wall -Wextra $NOWARNS -g "$OPT" ./main.c ./src/sc.c -lm -o sc
--- src/config.h
+++
@@ -1,9 +0,0 @@
-#ifndef __CONFIG_H__
-#define __CONFIG_H__
-
-#include <stdint.h>
-
-#define HEAP_SIZE UINT16_MAX
-#define ARR_GROW 64
-
-#endif
